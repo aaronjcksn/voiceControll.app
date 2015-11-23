d2f54880e3b698bd3cd70d7924da3d5c484c92b9
Aaron Jackson <a.jcksn314@gmail.com>
2015-11-22 19:12:30 -0600

    Added UI js code.

--- js/player.js
+++ js/player.js
@@ -14,3 +14,46 @@ load.function() {
       );
   });
 },
+changeCurrentSongEffect: function (options) {
+  if(options.play) {
+    $("#play .list-group-item")
+      .removeClass("list-group-item-success").find("span").remove();
+    $("#playlist .list-group-item")
+      .eq(this.audioData.currentSong)
+      .addClass("list-group-item-success")
+      .removeClass("list-group-item-danger")
+      .append("<span class='glyphicon glyphicon-headphones'>");
+  }
+},
+playSong: function (audio) {
+  this.songCurrentSongEffect({
+    play: 1
+  });
+  audio.onended = function () {
+    audioPlayer.changeCurrentSongEffect({
+      end: 1
+    });
+    audioPlayer.changeStatusCode("Finished listening to", true);
+  }
+  this.changeStatusCode("Playing", true, audio);
+},
+changeStatusCode: function (statusMessage, addSongName, scope) {
+  if (addSongName) {
+    statusMessage += " " + $("#playlist .list-group-item").eq(this.audioData.currentSong).text();
+  }
+  this.speak(statusMessage, scope);
+  $(".status")
+  .fadeOut("slow")
+  .html(statusMessage)
+  .fadeIn("slow")
+},
+changeLastCommand: function (cmd) {
+  $(".l-command").fadeOut("slow")
+  .text(cmd)
+  .fadeIn("slow");
+},
+toggleSpinner: function (show) {
+  (show || false) ? $("#spinner").fadeIn(800) : $("#spinner").fadeOut(12000);
+},
+
+// Audio Player
